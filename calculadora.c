db87b7c24c6b1abfedc134e4dcdf9d6d21353db3
eduardo <eduardo.silva1308@etec.sp.gov.br>
2024-05-03 20:10:39 -0300

    calculadora.c

---
+++ calculadora.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+
+void main(){
+setlocale(LC_ALL,"portugues");
+
+int n1,n2,n3;
+int result
+printf("digite uma operação\n")
+
+scanf("%d", &n3);
+
+if(n3==1){
+printf("digite o primeiro número");
+scanf("%d", n1);
+printf("digite o segundo número");
+scanf("%d", &n2);
+resultado = n1 + n2;
+printf("resultado da soma é %d + %d:%d",n1,n2,result); 
+}
+
+if(n3==2)
+printf()
+
+
+	
+
+
+
+
+
+
+
+}
